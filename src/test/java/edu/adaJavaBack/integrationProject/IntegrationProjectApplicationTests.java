19c3504e70ac3bfc07ebc93abb200ad04e6c9ee0
Andrés Felipe Rojas Ortiz <pipe@MacBook-Pro-de-Andres.local>
2022-03-14 12:13:22 -0500

    Initial commit

---
+++ src/test/java/edu/adaJavaBack/integrationProject/IntegrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.adaJavaBack.integrationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
